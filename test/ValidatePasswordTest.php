c745fe595bf1d20db695f86b1cd276e06dcba091
Anis Momin <anis_momin@hotmail.com>
2016-10-19 16:21:06 +0500

    added new method

--- test/ValidatePasswordTest.php
+++ test/ValidatePasswordTest.php
@@ -6,4 +6,9 @@ class ValidatePassword extends PHPUnit_Framework_TestCase {
 		$valPass = new ValidPassword();
 		$this->assertFalse($valPass->validLength('12345'));
 	}
+
+	public function testValidLength2(){
+		$valPass = new ValidPassword();
+		$this->assertFalse($valPass->validLength('12345'));
+	}
 }
